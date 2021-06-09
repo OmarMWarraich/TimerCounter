36d0b16069a11700440fa74b7ad4eba4b4f0f3c8
OmarMWarraich <earthling986@gmail.com>
2021-06-09 10:09:19 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import CountDownTimer from './CountDownTimer';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <CountDownTimer hours={1} minutes={20} seconds={40}/>
+      </div>
     </div>
   );
 }
